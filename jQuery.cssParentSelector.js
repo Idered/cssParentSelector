d4aea35317ea4b4dc1ba47a5e396605b62fd1237
Idered <IderedPL@gmail.com>
2012-01-17 16:36:29 +0100

    update

--- jQuery.cssParentSelector.js
+++ jQuery.cssParentSelector.js
@@ -7,78 +7,75 @@
  * Date 2011-12-29
  */
 (function($) {
-    $.fn.cssParentSelector = function() {
-    	var k = 0,
-    		state, child, selectors, selector, declarations, parent, apply
+  $.fn.cssParentSelector = function() {
+    var k = 0,
+      state, child, selectors, selector, declarations, parent, str,
 
-    		parse = function (css) {
+      parse = function(css) {
 
-	    	var i = -1,
-	    		j = 0,
-	    		parsed = '',
-				matches = css.match(/[a-zA-Z0-9#\.\,\-\n\r\t_:\[\]= ]*::parent[a-zA-Z0-9#\.\,\-\n\r\t_: ]*(\{[a-zA-Z0-9\s\/\.\,\-\n\r\t:#\(\);]*\})/g);
+        var i = j = -1,
+          parsed = '',
+          matches = css.match(/[a-zA-Z0-9#\.\,\-\n\r\t_:\[\]= ]*::parent[a-zA-Z0-9#\.\,\-\n\r\t_: ]*(\{[a-zA-Z0-9\s\/\.\,\-\n\r\t:#\(\);]*\})/g);
 
-			if (matches) for (;matches[++i], style = matches[i];) {
+        if (matches) {
+          for (; matches[++i], style = matches[i];) {
 
-				// returns everything that's before '{' and splits it by comma
-				selectors = style.split('{')[0].split(',');
+            // returns everything that's before '{' and splits it by comma
+            selectors = style.split('{')[0].split(',');
 
-				for (j = -1; selectors[++j], selector = $.trim(selectors[j]);) {
-					
-					// changed, selected, (disabled, enabled,)* checked, focus
-					state = selector.split('::parent')[0].split(/:|::/)[1];
+            for (j = -1; selectors[++j], str = $.trim(selectors[j]);) {
 
-					// does this selector have ::parent ?
-					apply = /::parent/.test(selector);
+              // changed, selected, (disabled, enabled,)* checked, focus
+              state = str.split('::parent')[0].split(/:|::/)[1];
 
-					child = $.trim(selector.split('::parent')[1]) || []._;
+              child = $.trim(str.split('::parent')[1]) || []._;
 
-					// float: right; etc.
-					declarations = style.split(/\{|\}/)[1].replace(/[\t\n\r]*/g, '');
+              // float: right; etc.
+              declarations = style.split(/\{|\}/)[1].replace(/[\t\n\r]*/g, '');
 
-					// p::parent => returns p
-					selector = selector.split(':')[0];
+              // p::parent => returns p
+              selector = str.split(':')[0];
 
-					if ( apply ) {
-						$(selector).each(function() {
-							var $this = $(this),
-								parent = $this.parent(),
-								id = 'cps' + k++,
-								toggleFn = function() {
-									$(parent).toggleClass(id);
-								};
+              // does this selector have ::parent ?
+              if (/::parent/.test(str)) {
+                $(selector).each(function() {
+                  var $this = $(this),
+                    parent = $this.parent(),
+                    id = 'cps' + k++,
+                    toggleFn = function() {
+                      $(parent).toggleClass(id);
+                    };
 
-							child && ( parent = parent.find(child) );
+                  child && (parent = parent.find(child));
 
-							!state ? toggleFn() : 
-							state == 'checked' ? $this.click(toggleFn) :
-							state == 'focus' ? $this.focus(toggleFn).blur(toggleFn) : 
-							state == 'selected' || state == 'changed' ? $this.change(toggleFn) : $this[state](toggleFn);
-							
-							parsed += '.' + id + '{' + declarations + '}';
-						});
-					} else {
-						parsed += selector + '{' + declarations + '}';
-					}
+                  !state ? 
+                  toggleFn() : state == 'checked' ? 
+                  $this.click(toggleFn) : state == 'focus' ? 
+                  $this.focus(toggleFn).blur(toggleFn) : state == 'selected' || state == 'changed' ? 
+                  $this.change(toggleFn) : $this[state](toggleFn);
 
-				}
-			};    			
-			
-			$('<style type="text/css">' + parsed + '</style>').appendTo('head');
+                  parsed += '.' + id + '{' + declarations + '}'
+                });
+              } else {
+                parsed += selector + '{' + declarations + '}'
+              }
 
-    	};
+            }
+          };
 
-    	$('link[rel=stylesheet], style').each(function() {
-    		if ($(this).is('link'))	{
-    			$.get(this.href).success(function(css) {
-    				parse (css);
-    			});
-    		} else {
-    			parse ( $(this).text() );
-    		}
-    	});
-    };
+          $('<style type="text/css">' + parsed + '</style>').appendTo('head')
+        }
 
-    $().cssParentSelector();
+      };
+
+    $('link[rel=stylesheet], style').each(function() {
+      $(this).is('link') ? $.get(this.href).success(function(css) {
+        parse(css)
+      }) : parse($(this).text())
+    });
+  };
+
+  // Autostart
+  $().cssParentSelector()
 
 })(jQuery);
